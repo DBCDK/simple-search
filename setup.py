51da7a6c0eadab22b23c82cd4cf278a1489d111f
Jesper Damkjær <jda@dbc.dk>
2020-07-03 12:50:32 +0200

    Randers pid list

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ setup(name="simple-search",
     packages=["simple_search", "simple_search.solr"],
     description="",
     provides=["simple_search"],
-    install_requires=["dbc-pyutils", "mobus", "tornado", "tqdm"],
+    install_requires=["booklens", "dbc-pyutils", "mobus", "tornado", "tqdm"],
     entry_points=
         {"console_scripts": [
             "simple-search-service = simple_search.service:main",
             "solr-indexer = simple_search.solr.indexer:main",
+            "pid-list-generator = simple_search.solr.pid_fetcher:main",
         ]}
     )
