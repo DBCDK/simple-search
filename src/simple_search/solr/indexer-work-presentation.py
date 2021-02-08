2d21f689aacb2608fea5390c376fa30b286b032c
Noah Torp-Smith <nots@dbc.dk>
2021-02-08 17:07:31 +0100

    work-presentation-data: control out filename

--- src/simple_search/solr/indexer-work-presentation.py
+++ src/simple_search/solr/indexer-work-presentation.py
@@ -292,7 +292,7 @@ def create_collection(solr_url, pid_list, work_to_holdings_map, pop_map, limit=N
     """
     logger.info("Retrieving data from db")
     documents = [d for d in make_solr_documents(pid_list, work_to_holdings_map, pop_map, limit)]
-    if len(solr_url > 0):
+    if len(solr_url > 5):
         logger.info(f"Indexing into solr at {solr_url}")
         indexer = ThreadedSolrIndexer(solr_url, num_threads=10, batch_size=batch_size)
         with Time("Indexing into solr took: ", level="info"):
@@ -302,7 +302,8 @@ def create_collection(solr_url, pid_list, work_to_holdings_map, pop_map, limit=N
         logger.info("Commit to solr done!")
         return
     else:
-        with open('output-data.json', 'w') as outfile:
+        s = 'output-json-' + solr_url
+        with open(s + '.json', 'w') as outfile:
             json.dump(documents, outfile)
         return
 
