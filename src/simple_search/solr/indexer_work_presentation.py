b2b59d1ba6452a072001a2847c6e08cdeac8d942
Noah Torp-Smith <nots@dbc.dk>
2021-02-10 11:01:58 +0100

    master: debug log

--- src/simple_search/solr/indexer_work_presentation.py
+++ src/simple_search/solr/indexer_work_presentation.py
@@ -167,10 +167,12 @@ def pwork2pids(pids) -> dict:
     """ Creates work -> (pids list) dict by fetching all relevant works from relations table in work-presentation-db """
     logger.info("fetching persistent workids for %d pids", len(pids))
     pw2p = {}
-    for row in get_docs(
+    docs = get_docs(
             "SELECT wc.manifestationid pid, wo.persistentworkid persistentworkid FROM workobject wo, workcontains wc WHERE wo.corepoworkid = wc.corepoworkid AND wc.manifestationid = ANY(SELECT pid FROM pids_tmp)",
             pids        
-        ):
+        )
+    logger.info("found %d docs in pwork2pids")
+    for row in docs:
         if row[1] in pw2p:
             pw2p[row[1]].append(row[0])
         else:
