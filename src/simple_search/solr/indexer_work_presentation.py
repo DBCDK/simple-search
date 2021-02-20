5773047161857c37e6f4fac167b5ef3e9d2ae7d8
Noah Torp-Smith <nots@dbc.dk>
2021-02-20 19:04:37 +0100

    master: revert debug

--- src/simple_search/solr/indexer_work_presentation.py
+++ src/simple_search/solr/indexer_work_presentation.py
@@ -165,19 +165,13 @@ def pwork2pids(pids) -> dict:
     """ Creates work -> (pids list) dict by fetching all relevant works from relations table in work-presentation-db """
     logger.info("fetching persistent workids for %d pids", len(pids))
     pw2p = {}
-    ## debug
-    counter = 0
-    for r in get_docs("SELECT wc.manifestationid pid, wo.persistentworkid persistentworkid FROM workobject wo, workcontains wc WHERE wo.corepoworkid = wc.corepoworkid AND wc.manifestationid = ANY(SELECT pid FROM pids_tmp)", pids):
-        counter = counter + 1
-    logger.info("counter is %d", counter)
-    ## end debug
     for row in get_docs("SELECT wc.manifestationid pid, wo.persistentworkid persistentworkid FROM workobject wo, workcontains wc WHERE wo.corepoworkid = wc.corepoworkid AND wc.manifestationid = ANY(SELECT pid FROM pids_tmp)", pids):
         if row[1] in pw2p:
             pw2p[row[1]].append(row[0])
         else:
             pw2p[row[1]] = [row[0]]
     res = dict(pw2p)
-    logger.info("pwork2pids will return dict of size %d", len(res))
+    logger.info("pwork2pids to return dict of size %d", len(res))
     return res
 
 def pid2corepo_work(pids) -> dict:
